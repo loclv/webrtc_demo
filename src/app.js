cf1d89cfb340c46945443ceab5692250a62ef502
loclv <masamonedante@gmail.com>
2020-07-08 19:12:45 +0900

    connect 2 peer

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 const openSteam = require('./openSteam');
+const Peer = require('simple-peer');
+const $ = require('jquery');
 
-openSteam();
+// openSteam();
+
+const p = new Peer({ initiator: location.hash === '#1', trickle: false });
+
+p.on('signal', token => {
+    $('#mySignalTxt').val(JSON.stringify(token));
+});
+
+p.on('connect', () => {console.log('connected!')});
+
+$('#connectBtn').click(() => {
+    const friendSignal = JSON.parse(
+        $('#friendSignal').val()
+    );
+    p.signal(friendSignal);
+});
 
 console.log('test');
