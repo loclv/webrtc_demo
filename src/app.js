e86169709228c529acdc9c385e09f4e102e5039e
loclv <masamonedante@gmail.com>
2020-07-09 18:15:33 +0900

    send stream

--- src/app.js
+++ src/app.js
@@ -1,26 +1,5 @@
 const openSteam = require('./openSteam');
-const Peer = require('simple-peer');
-const $ = require('jquery');
 
-// openSteam();
-
-const p = new Peer({ initiator: location.hash === '#1', trickle: false });
-
-p.on('signal', token => {
-    $('#mySignalTxt').val(JSON.stringify(token));
-});
-
-p.on('connect', () => {
-    setInterval(() => p.send(Math.random()), 1000);
-});
-
-p.on('data', data => {console.log(`received data: ${data}`);});
-
-$('#connectBtn').click(() => {
-    const friendSignal = JSON.parse(
-        $('#friendSignal').val()
-    );
-    p.signal(friendSignal);
-});
+openSteam();
 
 console.log('test');
--- src/openSteam.js
+++ src/openSteam.js
@@ -1,8 +1,36 @@
 const playVideo = require('./playVideo');
+const Peer = require('simple-peer');
+const $ = require('jquery');
 
 function openSteam() {
     navigator.mediaDevices.getUserMedia({ audio: true, video: true})
-    .then(stream => playVideo(stream, 'localStream'))
+    .then(stream => {
+        playVideo(stream, 'localStream');
+        const p = new Peer({
+            initiator: location.hash === '#1',
+            trickle  : false,
+            stream
+        });
+
+        p.on('signal', token => {
+            $('#mySignalTxt').val(JSON.stringify(token));
+        });
+
+        // p.on('connect', () => {
+        //     setInterval(() => p.send(Math.random()), 1000);
+        // });
+
+        p.on('stream', friendStream => {
+            playVideo(friendStream, 'friendStream');
+        });
+
+        $('#connectBtn').click(() => {
+            const friendSignal = JSON.parse(
+                $('#friendSignal').val()
+            );
+            p.signal(friendSignal);
+        });
+    })
     .catch(error => console.log(error));
 }
 
